f5de28c7ebb15fc4a6658ec81c632bcfbca50373
vishalJoshiVJ00 <vishaljoshivj00100@gmail.com>
2025-09-24 22:14:34 +0530

    captial first letter after dot(.) and count space

--- string/paragraph_with_count_space.py
+++ string/paragraph_with_count_space.py
@@ -1,8 +1,36 @@
-paragraph = input("enter the paragraph")
-paragraph_to_list = paragraph.split(" ")
-for i in range(len(paragraph)):
-    if paragraph[i]==".":
-        paragraph[i+2].upper()
-space_of_paragraph = len(paragraph_to_list) - 1
-print(paragraph)
-print(space_of_paragraph)
+# Program: Capitalize first letter after '.' and count spaces in a paragraph
+
+# Take paragraph input from user
+paragraph = input("Enter the paragraph: ")
+
+# Variable to count spaces
+space_count = 0
+
+# Convert string to list of characters (strings are immutable in Python)
+char_list = list(paragraph)
+
+# Loop through characters
+for i in range(len(char_list) - 2):   # -2 to avoid index error
+    # If character is a full stop
+    if char_list[i] == '.':
+        # Case 1: dot followed by a space
+        if char_list[i+1] == ' ':
+            char_list[i+2] = char_list[i+2].upper()
+        # Case 2: dot directly followed by a letter
+        else:
+            char_list[i+1] = char_list[i+1].upper()
+    
+    # Count spaces
+    if char_list[i] == ' ':
+        space_count += 1
+
+# Check last character for space
+if char_list[-1] == ' ':
+    space_count += 1
+
+# Convert list back to string
+modified_paragraph = "".join(char_list)
+
+# Print results
+print("Modified paragraph:", modified_paragraph)
+print("Number of spaces:", space_count)
